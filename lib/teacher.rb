7973230c21d4b5b0a7f9f89da6272356ae7d916b
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-22 15:37:21 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,6 +3,6 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
-    puts "sample () method form : #{KNOWLEDGE.sample(1)}\n\n"
+    return "sample () method form : #{knowledge.sample(1)}\n\n"
   end
 end
